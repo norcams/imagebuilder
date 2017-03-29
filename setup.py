cc39c869783c459e97a9faf1e95e6556961bb122
Anders Vaage <anders@unix.uib.no>
2017-03-29 11:06:19 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,21 @@
 from setuptools import setup
 import image_builder
 
-deps = ['keystoneauth1', 'python-novaclient']
 
 setup(
-      name=image_builder.__productname__,
-      description=image_builder.__description__,
-      version=image_builder.__version__,
-      author=image_builder.__author__,
-      author_email=image_builder.__author_email__,
-      url=image_builder.__url__,
-      requires=deps,
-      install_requires=deps,
-      packages=['image_builder'],
-      entry_points={
-          'console_scripts': [
-              'imagebuilder = image_builder.imagebuilder:main'
-          ]
-      }
-     )
+    name=image_builder.__productname__,
+    description=image_builder.__description__,
+    version=image_builder.__version__,
+    author=image_builder.__author__,
+    author_email=image_builder.__author_email__,
+    url=image_builder.__url__,
+    install_requires=["keystoneauth1",
+                      "python-novaclient",
+                      "python-glanceclient"],
+    packages=['image_builder'],
+    entry_points={
+        'console_scripts': [
+            'imagebuilder = image_builder.imagebuilder:main'
+            ]
+        }
+    )
