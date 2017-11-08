756273b9927523f7cc3e094488ae065d77543436
Anders Vaage <anders@unix.uib.no>
2017-11-08 15:18:57 +0100

    make sure upgrade provision script doesn't get stuck when installing packages with interactive prompts on ubuntu and debian

--- scripts/upgrade.sh
+++ scripts/upgrade.sh
@@ -39,7 +39,7 @@ case $platform in
     ;;
   "debian")
     sudo apt-get update \
-    && sudo apt-get dist-upgrade -y \
+    && DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade \
     && sudo apt-get autoremove -y; \
     sudo apt-get clean; \
     sudo rm -rf /var/lib/apt/lists/*; \
@@ -47,7 +47,7 @@ case $platform in
     ;;
   "ubuntu")
     sudo apt-get update \
-    && sudo apt-get dist-upgrade -y \
+    && DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade \
     && sudo apt-get autoremove -y; \
     sudo apt-get clean; \
     sudo rm -rf /var/lib/apt/lists/*; \
