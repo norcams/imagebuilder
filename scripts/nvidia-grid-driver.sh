a9ab459e6fae402bb70fb37a6d8a546b6f7cc61b
Trond Hasle Amundsen <t.h.amundsen@usit.uio.no>
2021-07-07 19:36:15 +0200

    bugfix for nvidia script

--- scripts/nvidia-grid-driver.sh
+++ scripts/nvidia-grid-driver.sh
@@ -6,6 +6,9 @@
 # May work on other distros like Fedora and Debian
 #
 
+# Set PATH
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin
+
 # Be verbose, disallow unset variables and exit on error
 set -x
 set -u
@@ -37,6 +40,11 @@ elif [ -f "/etc/debian_version" ]; then
 elif [ -f "/etc/redhat-release" ]; then
     platform=$(sed 's/^\(.\+\) release.*/\1/' /etc/redhat-release | tr '[A-Z]' '[a-z]')
     platform_version=$(sed 's/^.\+ release \([.0-9]\+\).*/\1/' /etc/redhat-release)
+
+    # If /etc/redhat-release exists, we act like RHEL by default
+    if [ "$platform" != "fedora" ]; then
+	platform="el"
+    fi
 fi
 
 if [ "x$platform" = "x" ]; then
@@ -49,71 +57,48 @@ major_version=$(echo $platform_version | cut -d. -f1)
 # The nouveau driver needs to be blacklisted
 sudo sh -c "printf 'blacklist nouveau\noptions nouveau modeset=0\n' > /etc/modprobe.d/blacklist-nvidia-nouveau.conf"
 case $platform in
-    "fedora")
-	sudo /usr/bin/dracut --force
+    'fedora'|'el')
+	sudo dracut --force
 	;;
-    "el")
-	case $major_version in
-	    "7")
-		sudo /sbin/dracut --force
-		;;
-	    "8")
-		sudo /usr/bin/dracut --force
-		;;
-	esac
-	;;
-    "debian"|"ubuntu")
-	sudo /usr/sbin/update-initramfs -u
+    'debian'|'ubuntu')
+	sudo update-initramfs -u
 	;;
 esac
 
 # Install dkms and pciutils. Then determine newest installed kernel
 case $platform in
-    "fedora")
-	sudo /usr/bin/dnf install -y pciutils dkms kernel-devel kernel-headers
-	sudo /usr/bin/dnf update -y kernel kernel-devel kernel-headers
-	KERNELINSTALLED=$(rpm -qa kernel | sort -V -r | head -n 1)
-	KERNELVERSION=${KERNELINSTALLED##kernel-}
+    'fedora')
+	sudo dnf install -y pciutils dkms kernel-devel kernel-headers
+	sudo dnf upgrade -y kernel kernel-devel kernel-headers
+	KERNELVERSION=$(grubby --default-kernel | sed 's|/boot/vmlinuz-||')
 	;;
-    "el")
-	case $major_version in
-	    "7")
-		sudo /bin/yum install -y pciutils epel-release
-		sudo /bin/yum install -y dkms kernel-devel kernel-headers
-		sudo /bin/yum update -y kernel kernel-devel kernel-headers
-		KERNELINSTALLED=$(rpm -qa kernel | sort -V -r | head -n 1)
-		KERNELVERSION=${KERNELINSTALLED##kernel-}
-		;;
-	    "8")
-		sudo /usr/bin/dnf install -y pciutils epel-release
-		sudo /usr/bin/dnf install -y dkms kernel-devel kernel-headers
-		sudo /usr/bin/dnf update -y kernel kernel-devel kernel-headers
-		KERNELINSTALLED=$(rpm -qa kernel | sort -V -r | head -n 1)
-		KERNELVERSION=${KERNELINSTALLED##kernel-}
-		;;
-	esac
+    'el')
+	sudo yum install -y pciutils epel-release
+	sudo yum install -y dkms kernel-devel kernel-headers
+	sudo yum upgrade -y kernel kernel-devel kernel-headers
+	KERNELVERSION=$(grubby --default-kernel | sed 's|/boot/vmlinuz-||')
 	;;
-    "debian")
-	sudo /usr/bin/apt-get update -y
-	sudo /usr/bin/apt-get dist-upgrade -y
-	sudo /usr/bin/apt -y install dkms pciutils curl
-	KERNELINSTALLED=$(/usr/bin/dpkg --list | grep linux-image | grep -v meta-package | sort -V -r | head -n 1 | cut -d' ' -f3)
+    'debian')
+	sudo apt-get update -y
+	sudo apt-get dist-upgrade -y
+	sudo apt -y install dkms pciutils curl
+	KERNELINSTALLED=$(dpkg --list | grep linux-image | grep -v meta-package | sort -V -r | head -n 1 | cut -d' ' -f3)
 	KERNELVERSION=${KERNELINSTALLED##linux-image-}
-	sudo /usr/bin/apt install -y linux-headers-$KERNELVERSION
+	sudo apt install -y linux-headers-$KERNELVERSION
 	;;
-    "ubuntu")
-	sudo /usr/bin/apt-get update -y
-	sudo /usr/bin/apt-get dist-upgrade -y
-	sudo /usr/bin/apt -y install dkms pciutils
-	KERNELINSTALLED=$(/usr/bin/dpkg --list | grep linux-image | grep generic | sort -V -r | head -n 1 | cut -d' ' -f3)
+    'ubuntu')
+	sudo apt-get update -y
+	sudo apt-get dist-upgrade -y
+	sudo apt -y install dkms pciutils
+	KERNELINSTALLED=$(dpkg --list | grep linux-image | grep generic | sort -V -r | head -n 1 | cut -d' ' -f3)
 	KERNELVERSION=${KERNELINSTALLED##linux-image-}
-	sudo /usr/bin/apt install -y linux-headers-$KERNELVERSION
+	sudo apt install -y linux-headers-$KERNELVERSION
 	;;
 esac
 
 # Get latest NVIDIA GRID package and build with dkms for newest installed kernel
 cd /tmp
-/usr/bin/curl -O https://download.iaas.uio.no/nrec/nrec-resources/files/nvidia-vgpu/linux-grid-latest
+curl -O https://download.iaas.uio.no/nrec/nrec-resources/files/nvidia-vgpu/linux-grid-latest
 chmod +x linux-grid-latest
 sudo ./linux-grid-latest --dkms -n -s -k $KERNELVERSION
 
@@ -127,4 +112,4 @@ fi
 
 # Clean up the driver package
 cd /tmp
-/bin/rm -f ./linux-grid-latest
+rm -f ./linux-grid-latest
