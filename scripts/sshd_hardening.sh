0923365c9070a43b018b75e1b4f44b3b6dafdfd1
Trond Hasle Amundsen <trondham@uio.no>
2022-06-27 12:33:42 +0200

    add script for sshd hardening

---
+++ scripts/sshd_hardening.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Set proper PATH
+PATH=/usr/bin:/usr/sbin
+export PATH
+
+# Variables
+SSHD_CONF=/etc/sshd_config
+SSHD_TMP=$(mktemp)
+
+# Add desired hardening block, creating temporary file
+awk '
+{ print }
+/#ListenAddress ::/ {
+    print ""
+    print "# Host Keys [NREC]"
+    print "HostKey /etc/ssh/ssh_host_rsa_key"
+    print "HostKey /etc/ssh/ssh_host_ed25519_key"
+    print ""
+    print "# OpenSSH hardening [NREC]"
+    print "KexAlgorithms curve25519-sha256@libssh.org,diffie-hellman-group-exchange-sha256"
+    print "Ciphers chacha20-poly1305@openssh.com,aes256-gcm@openssh.com,aes128-gcm@openssh.com,aes256-ctr,aes128-ctr"
+    print "MACs hmac-sha2-512-etm@openssh.com,hmac-sha2-256-etm@openssh.com,umac-128-etm@openssh.com,hmac-sha2-512,hmac-sha2-256,umac-128@openssh.com"
+    print "HostkeyAlgorithms ssh-ed25519,ssh-rsa"
+}
+' $SSHD_CONF > $SSHD_TMP
+
+# Replace with edited file
+mv -f $SSHD_TMP $SSHD_CONF
